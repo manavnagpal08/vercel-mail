b66982347502f274a7f98e3908f8ea8df1dd621b
Manav Nagpal <manavnagpal08@gmail.com>
2026-05-17 08:15:07 +0530

    Fix Vercel free tier cron limit

--- push.ps1
+++
@@ -1,6 +0,0 @@
-git init
-git add .
-git commit -m "Update vercel mail server"
-git branch -M main
-git remote add origin https://github.com/manavnagpal08/vercel-mail.git
-git push -u origin main -f
